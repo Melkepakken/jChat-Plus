efb05b278f1c0da85b0e26da0b345973a684500b
Naith <melkepakken@gmail.com>
2026-06-28 08:25:32 +0200

    Add Kick chat and modernize jChat integrations
    Kick chat messages should now work with jChat if you add your Kick Chatroom ID to the URL (&kick_room=[YOUR_ROOM_ID]). If I'm bothered enough, I'll make a proper username lookup to find room ID based on username in the future.
    
    Also, the severly outdated jChat repo used v5 of Twitch API, and old 7TV API. This has all been scrapped and replaced with Helix and v3 for 7TV.
    
    I also updated the !reloadoverlay to be !reloadchat as in 3.0.1. Fixed some other annoyances as well like 7TV badges, FFZ room 404 errors, bits rendering, twitch badges. Should be quite decent now. Still, there's some things left to do here.
    
    For local self-hosting, you'll need credentials.js with client_id and oauth.

--- v2/utils.js
+++ v2/utils.js
@@ -1,23 +1,35 @@
 function appendCSS(type, name) {
-    $("<link/>", {
-        rel: "stylesheet",
-        type: "text/css",
-        class: `chat_${type}`,
-        href: `styles/${type}_${name}.css`
-    }).appendTo("head");
+  $("<link/>", {
+    rel: "stylesheet",
+    type: "text/css",
+    class: `chat_${type}`,
+    href: `styles/${type}_${name}.css`,
+  }).appendTo("head");
 }
 
-function escapeRegExp(string) { // Thanks to coolaj86 and Darren Cook (https://stackoverflow.com/a/6969486)
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+function escapeRegExp(string) {
+  // Thanks to coolaj86 and Darren Cook (https://stackoverflow.com/a/6969486)
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
 function escapeHtml(message) {
-    return message
-        .replace(/&/g, "&amp;")
-        .replace(/(<)(?!3)/g, "&lt;")
-        .replace(/(>)(?!\()/g, "&gt;");
+  return message
+    .replace(/&/g, "&amp;")
+    .replace(/(<)(?!3)/g, "&lt;")
+    .replace(/(>)(?!\()/g, "&gt;");
 }
 
 function TwitchAPI(url) {
-    return $.getJSON(url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=' + client_id);
+  if (url.includes("api.twitch.tv/helix")) {
+    return $.ajax({
+      url: url,
+      dataType: "json",
+      headers: {
+        "Client-ID": client_id,
+        Authorization: "Bearer " + oauth_token,
+      },
+    });
+  }
+
+  return $.getJSON(url);
 }
